1dff636dc56bfdd670992d121b08786f75a9c362
ko25252929 <76517744+ko25252929@users.noreply.github.com>
2021-02-16 16:11:27 +0900

    SNS認証実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :tweets
   has_many :comments
+  has_many :sns_credentials
   
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
